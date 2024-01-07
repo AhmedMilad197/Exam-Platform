21a700ab52b897a0654f1bda47a33b1f5eb76abc
AhmedMilad197 <ahmilad197@gmail.com>
2024-01-08 00:54:37 +0200

    modified the make question form

--- Client/src/components/Questions.vue
+++ Client/src/components/Questions.vue
@@ -2,25 +2,54 @@
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
-const route = useRoute()
+const route = useRoute();
+const question = ref(null);
+const currentQuestion = ref();
+const answers = ref([]);
+const currentAnswer = ref();
+const selectedItems = [];
 
-// This will be loaded from the database.
-const answers = ref([
-  'First Answer',
-  'Second Answer',
-  'Third Answer',
-  'Fourth Answer',
-])
+function addQuestion () {
+  question.value = currentQuestion.value;
+  console.log(question.value);
+}
 
-const selectedItems = []
+function addSelected (answer) {
+  selectedItems.push(answer);
+}
 
-function addOrDelete (answer) {
+function deleteSelected (answer) {
   var index = selectedItems.indexOf(answer);
   if (index !== -1) {
     selectedItems.splice(index, 1);
-  } else {
-    selectedItems.push(answer);
   }
+}
+
+function addOrDelete (answer) {
+    var index = selectedItems.indexOf(answer);
+    if (index !== -1) {
+      deleteSelected(answer);
+    } else {
+      addSelected(answer);
+    }
+    console.log(selectedItems);
+}
+
+function addAnswer() {
+  if (currentAnswer != '') {
+    answers.value.push(currentAnswer.value);
+    currentAnswer.value = '';
+  }
+}
+
+function deleteAnswer(answer) {
+  var index = answers.value.indexOf(answer);
+  if (index !== -1) {
+    deleteSelected(answer);
+    answers.value.splice(index, 1);
+    currentAnswer.value = '';
+  }
+  console.log(selectedItems);
 }
 
 </script>
@@ -36,32 +65,43 @@ function addOrDelete (answer) {
 
     <v-list lines="three" select-strategy="classic">
       <v-sheet class="text-h5 mx-4 my-4">
-        What is the question here??
+        <div v-if="question !== null">
+          {{ question }}
+        </div>
+        <div v-else>
+          <div class="d-flex flex-row">
+            <v-textarea label="Question" auto-grow class="my-auto ml-4" v-model="currentQuestion"></v-textarea>
+            <v-btn color="success" class="my-auto mx-2" @click="addQuestion()">Add Question</v-btn>
+          </div>
+        </div>
       </v-sheet> <hr>
-      <div v-for="(answer, index) in answers" :key="index">
-        <v-list-item :value="index" @click="addOrDelete(answer)">
-          
+
+      <div v-for="(answer, index) in answers" :key="index" class="d-flex">
+        <v-list-item :value="index" @click="addOrDelete(answer)" width="85%">
           <template v-slot:prepend="{ isActive }">
             <v-list-item-action start>
-              <v-checkbox-btn v-model="selectedItems" color="success" :model-value="isActive"></v-checkbox-btn> 
+              <v-checkbox-btn color="success" :model-value="isActive"></v-checkbox-btn> 
             </v-list-item-action>
           </template>
-          
-          <v-sheet class="text-h6">
+          <div class="d-flex text-h6">
             {{ answer }}
-          </v-sheet>
-  
+          </div>
         </v-list-item>
+        <v-btn color="red" class="my-auto mx-auto" @click="deleteAnswer(answer)">Delete</v-btn>
       </div>
-
     </v-list>
-    <div class="d-flex flex-row">
-      <v-textarea label="Answer" auto-grow class="my-auto ml-4"></v-textarea>
-      <v-btn color="success" class="my-auto mx-2">Add Answer</v-btn>
+  
+    <div v-if="question">
+      <div class="d-flex flex-row">
+        <v-textarea label="Answer" auto-grow class="my-auto ml-4" v-model="currentAnswer"></v-textarea>
+        <v-btn color="success" class="my-auto mx-2" @click="addAnswer()">Add Answer</v-btn>
+      </div>
     </div>
+
     <div class="d-flex flex-row">
       <v-btn color="primary" class="mx-auto my-4">Add Question</v-btn>
     </div>
+  
   </v-card>
 </template>
 
