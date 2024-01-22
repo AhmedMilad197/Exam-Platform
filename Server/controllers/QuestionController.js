f7afcf02f774ad6bb84e99943233355e05ae8387
AhmedMilad197 <ahmilad197@gmail.com>
2024-01-23 00:42:23 +0200

    modified the Edit question view

--- Server/controllers/QuestionController.js
+++ Server/controllers/QuestionController.js
@@ -3,7 +3,29 @@ module.exports = {
     await res.send({
       body: {
         id: req.body.id,
-        question: 'Question from the backend'
+        question: 'Question from the backend',
+        answers: [
+          {
+            id: 1,
+            value: 'This is the first answer',
+            isAnswer: false
+          },
+          {
+            id: 2,
+            value: 'This is the second answer',
+            isAnswer: true
+          },
+          {
+            id: 3,
+            value: 'This is the third answer',
+            isAnswer: false
+          },
+          {
+            id: 4,
+            value: 'This is the fourth answer',
+            isAnswer: false
+          },
+        ]
       }
     });
   }
