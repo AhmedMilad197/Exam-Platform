a69bdd2387e88dc4e12e9fa604334d1fa3944153
AhmedMilad197 <ahmilad197@gmail.com>
2024-03-29 11:03:15 +0200

    modified exam details view

--- Client/src/components/ViewExam.vue
+++ Client/src/components/ViewExam.vue
@@ -10,6 +10,7 @@ const description = ref();
 const start_time = ref();
 const end_time = ref();
 const questions = ref();
+const dialog = ref(false);
 
 function navigateTo (path) {
   router.push(path);
@@ -33,6 +34,18 @@ async function getExam () {
   }
 }
 
+async function destroy() {
+  try {
+    const response = await ExamService.delete(route.params.id)
+    dialog.value = false;
+    router.go(-1);
+  } catch (error) {
+    return {
+      message: error.message
+    }
+  }
+}
+
 onMounted(() => {
   getExam();
 });
@@ -40,81 +53,15 @@ onMounted(() => {
 </script>
 
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="1000"
-    max-height="100vh"
-    >
-    <v-toolbar color="purple">
-      <v-toolbar-title>Subject Details</v-toolbar-title>
-    </v-toolbar>
-    <v-table
-      fixed-header
-      height="100%"
-      density="comfortable"
-    >
-      <thead>
-        <tr>
-          <th class="text-left">
-            DATA
-          </th>
-          <th class="text-left">
-            VALUE
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr style="height: 50px;">
-          <td>
-            <b>
-              Exam Title
-            </b>
-          </td>
-          <td>
-            {{ name }}
-          </td>
-        </tr>
-        <tr style="height: 50px;">
-          <td>
-            <b>
-              Exam Description
-            </b>
-          </td>
-          <td>
-            {{ description }}
-          </td>
-        </tr>
-        <tr style="height: 50px;">
-          <td>
-            <b>
-              Start Time
-            </b>
-          </td>
-          <td>
-            {{ start_time }}
-          </td>
-        </tr>
-        <tr style="height: 50px;">
-          <td>
-            <b>
-              End Time
-            </b>
-          </td>
-          <td>
-            {{ end_time }}
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
-    <hr>
-    <div>
-      <div class="mx-4 my-4">
-        <span class="text-h5 mx-4 my-4">
-          Questions
-        </span>
-      </div>
-    </div>
-    <div>
+  <v-locale-provider rtl>
+    <v-card
+      class="mx-auto"
+      max-width="1000"
+      max-height="100vh"
+      >
+      <v-toolbar>
+        <v-toolbar-title>بيانات الإختبار</v-toolbar-title>
+      </v-toolbar>
       <v-table
         fixed-header
         height="100%"
@@ -122,42 +69,119 @@ onMounted(() => {
       >
         <thead>
           <tr>
-            <th class="text-left">
-              Question
+            <th class="text-rigth">
+              البيانات
             </th>
-            <th class="text-center">
-              Actions
+            <th class="text-rigth">
+              القيمة
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="question in questions"
-            :key="question.id"
-            style="height: 50px;"
-          >
-          <td>
-            {{ question.content }}
-          </td>
+          <tr style="height: 50px;">
             <td>
-              <div class="d-flex">
-                <div class="mx-auto">
-                  <v-btn color="indigo-darken-3" @click="navigateTo({name: 'view-question', params: {id: question.id}})">VIEW</v-btn>
-                </div>
-              </div>
+              <b>
+                عنوان الإختبار
+              </b>
+            </td>
+            <td>
+              {{ name }}
+            </td>
+          </tr>
+          <tr style="height: 50px;">
+            <td>
+              <b>
+                وصف الإختبار
+              </b>
+            </td>
+            <td>
+              {{ description }}
+            </td>
+          </tr>
+          <tr style="height: 50px;">
+            <td>
+              <b>
+                وقت البدء
+              </b>
+            </td>
+            <td>
+              {{ start_time }}
+            </td>
+          </tr>
+          <tr style="height: 50px;">
+            <td>
+              <b>
+                وقت الإنتهاء
+              </b>
+            </td>
+            <td>
+              {{ end_time }}
             </td>
           </tr>
         </tbody>
       </v-table>
-    </div>
-    <div class="d-flex">
-      <!-- <v-spacer></v-spacer> -->
-      <div class="mx-auto">
-        <v-btn color="red" class="my-4 mx-2">DELETE</v-btn>
-        <v-btn color="primary" class="my-4 mx-2" @click="router.go(-1)">GO BACK</v-btn>
+      <hr>
+      <div>
+        <div class="mx-4 my-4">
+          <span class="text-h5 mx-4 my-4">
+            الأسئلة
+          </span>
+        </div>
       </div>
-    </div>
-  </v-card>
+      <div>
+        <v-table
+          fixed-header
+          height="100%"
+          density="comfortable"
+        >
+          <thead>
+            <tr>
+              <th class="text-right">
+                السؤال
+              </th>
+              <th class="text-center">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="question in questions"
+              :key="question.id"
+              style="height: 50px;"
+            >
+            <td>
+              {{ question.content }}
+            </td>
+              <td>
+                <div class="d-flex">
+                  <div class="mx-auto">
+                    <v-btn color="indigo-darken-3" @click="navigateTo({name: 'view-question', params: {id: question.id}})">عرض</v-btn>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </div>
+      <div class="d-flex">
+        <div class="mx-auto">
+          <v-btn color="red" class="my-4 mx-2" @click="dialog = true">حذف</v-btn>
+          <v-btn color="primary" class="my-4 mx-2" @click="router.go(-1)">العودة</v-btn>
+        </div>
+      </div>
+    </v-card>
+    <v-dialog v-model="dialog" width="auto">
+      <v-card max-width="400" prepend-icon="mdi-alert-circle"
+        text="هل تريد حذف هذا الإختبار؟"
+        title="تأكيد">
+        <template v-slot:actions>
+          <v-btn color="red" @click="destroy()">نعم</v-btn>
+          <v-btn color="primary" @click="dialog = false">العودة</v-btn>
+        </template>
+      </v-card>
+    </v-dialog>
+  </v-locale-provider>
 </template>
 
 <style scoped>
